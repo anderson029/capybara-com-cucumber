17e07644c367bf4ccbfd142c39e0095a34c1e829
Anderson Ferreira <andersonfoliveira@yahoo.com>
2020-08-21 08:46:11 -0300

    configurando capybara e drivers

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,2 +1,11 @@
 require 'capybara/cucumber'
 require 'selenium-webdriver'
+
+Capybara.configure|config|
+#Capybara disponibiliza 3 drivers padrão de configuração: selenium(firefox) , selenium_chrome, selenium_chrome_hedless(sem gpu- não sobe o chrome)
+config.default_driver = :selenium_chrome
+#configurando url padrão
+config.app_host = 'https://automacaocombatista.herokuapp.com/'
+#configurando o tempo de espera dos elementos na tela
+config.default_max_wait_time = 5
+end
