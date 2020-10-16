eacc1dda2d5457f07046436e21bfb28aa28506f4
Anderson Ferreira <andersonfoliveira@yahoo.com>
2020-10-16 13:46:58 -0300

    ajuste

--- features/step_definitions/buscando_elemento.rb
+++ features/step_definitions/buscando_elemento.rb
@@ -4,8 +4,8 @@ end
 
 Então('verifico se encontrei os elementos') do
   page.all(:css,'.btn') # all= busca todos os elementos que contém all na página com a nova atualização não precisa usar o ':css'
-  find('#teste').click # find= busca um elemento mapeado(único)
-  find_by_id('teste') # busca pelo id
+  find('#teste').click # buscando por id "css"
+  find_by_id('teste') # busca pelo id direto no capybara
   find_button(class:'btn waves-light') # busca pela class
   first('.btn') # busca o primeiro elemento que está mapeado
   find_link(href: 'https://www.youtube.com/channel/UCp554v_is_ZzjzSmAQyFfAA')# busca por um link
